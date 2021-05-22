999367cfcc2196ea7583170b00ebe8ef8c982249
Christine Eum <eumc@uci.edu>
2021-05-22 16:02:00 +0900

    removebutton

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -103,6 +103,16 @@ socket.on('player_disconnected', (payload) => {
         console.log('Server did not send a payload');
         return;
     }
+
+    if(payload.socket_id === socket.id){
+        return;
+    }
+
+    let domElements = $('socket_'+payload.socket_id);
+    if(domElements.length !== 0)
+        domElements.hide("fade",500);
+    }    
+
     let newHTML = '<p class=\'left_room_response\'>' + payload.username + ' left the ' + 
     payload.room+ '. (There are '+ payload.count + ' users in this room)</p>';
     let newNode = $(newHTML);
