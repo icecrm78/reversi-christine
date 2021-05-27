f1272a3e29b5c9dd5e47bd967559bf5f5e2844c2
Christine Eum <eumc@uci.edu>
2021-05-27 09:33:45 +0900

    game start

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -56,9 +56,17 @@ function makeInvitedButton(socket_id) {
     return newNode;
 }
 
-function makePlayButton() {
+function makePlayButton(socket_id) {
     let newHTML= "<button type='button' class='btn btn-success'>Play</button>";
     let newNode = $(newHTML);
+    newNode.click( () => {
+        let payload = {
+            requested_user:socket_id
+        }
+        console.log('**** Client log message, sending \'game_start\' command: '+JSON.stringify(payload));
+        socket.emit('game_start',payload);
+    }
+    );
     return newNode;
 }
 
@@ -108,6 +116,22 @@ socket.on('uninvited', (payload) => {
     let newNode = makeInviteButton(payload.socket_id);
     $('.socket_' + payload.socket_id + ' button').replaceWith(newNode);
 })
+
+
+socket.on('game_start_response', (payload) => {
+    if ((typeof payload == 'undefined') || (payload === null)) {
+        console.log('Server did not send a payload');
+        return;
+    }
+    if (payload.result === 'fail') {
+        console.log(payload.message);
+        return;
+    } 
+    let newNode = makeStartGameButton();
+    $('.socket_' + payload.socket_id + ' button').replaceWith(newNode);
+    /* Jump to the game page */
+    window.location.href = 'game.html?username=' + username + '&game_id=' + payload.game.id;
+});
 
 
 socket.on('join_room_response', (payload) => {
